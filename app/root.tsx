490a07c88d3dc0a68c20f0bc9475ef5275e8a4ae
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-03-11 19:05:29 +0200

    Install @clerk/remix

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,25 @@
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "remix";
 import type { MetaFunction } from "remix";
+import { Links, LiveReload, LoaderFunction, Meta, Outlet, Scripts, ScrollRestoration } from "remix";
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+import { ConnectClerk, ConnectClerkCatchBoundary } from "@clerk/remix";
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
 
-export default function App() {
+export const loader: LoaderFunction = (args) => {
+  return rootAuthLoader(
+    args,
+    ({ auth }) => {
+      const { userId, sessionId, getToken } = auth;
+      console.log("Root loader auth:", { userId, sessionId, getToken });
+      return { message: `Hello from the root loader :)` };
+    },
+    { loadUser: true }
+  );
+};
+
+function App() {
   return (
     <html lang="en">
       <head>
@@ -23,3 +37,7 @@ export default function App() {
     </html>
   );
 }
+
+export default ConnectClerk(App);
+
+export const CatchBoundary = ConnectClerkCatchBoundary();
